b6adbd112319cf8be5730bd652defec00a2815ab
Jarod Castillo <43973221+Smotto@users.noreply.github.com>
2022-12-01 03:18:40 -0800

    added material ui and top buttons

--- Squarl/ViewModels/MainWindowViewModel.cs
+++ Squarl/ViewModels/MainWindowViewModel.cs
@@ -2,5 +2,11 @@
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public string FileText => "File";
+    public string EditText => "Edit";
+    public string LayoutText => "Layout";
+    public string WindowsText => "Windows";
+    public string ScansText => "Scans";
+    public string DebuggerText = "Debugger";
+
 }

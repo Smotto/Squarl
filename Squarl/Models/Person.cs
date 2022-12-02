48d64f4bb712603d3ac5638c384d8bcaf0fa0c66
Jarod Castillo <43973221+Smotto@users.noreply.github.com>
2022-12-01 17:22:28 -0800

    removed Material Theme, added tree

---
+++ Squarl/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Squarl.Models;
+
+public class Person
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public int Age { get; set; }
+    public ObservableCollection<Person> Children { get; } = new();
+}
--- Squarl/ViewModels/MainWindowViewModel.cs
+++ Squarl/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,31 @@
-﻿namespace Squarl.ViewModels;
+﻿using System.Collections.ObjectModel;
+using Avalonia.Controls;
+using Avalonia.Controls.Models.TreeDataGrid;
+using Squarl.Models;
+
+namespace Squarl.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string FileText => "File";
-    public string EditText => "Edit";
-    public string LayoutText => "Layout";
-    public string WindowsText => "Windows";
-    public string ScansText => "Scans";
-    public string DebuggerText = "Debugger";
-
+    private ObservableCollection<Person> _people = new()
+    {
+        new Person { FirstName = "Eleanor", LastName = "Pope", Age = 32 },
+        new Person { FirstName = "Jeremy", LastName = "Navarro", Age = 74 },
+        new Person { FirstName = "Lailah ", LastName = "Velazquez", Age = 16 },
+        new Person { FirstName = "Jazmine", LastName = "Schroeder", Age = 52 },
+    };
+    
+    public MainWindowViewModel()
+    {
+        Source = new FlatTreeDataGridSource<Person>(_people)
+        {
+            Columns =
+            {
+                new TextColumn<Person, string>("First Name", x => x.FirstName),
+                new TextColumn<Person, string>("Last Name", x => x.LastName),
+                new TextColumn<Person, int>("Age", x => x.Age),
+            },
+        };
+    }
+    public FlatTreeDataGridSource<Person> Source { get; }
 }
